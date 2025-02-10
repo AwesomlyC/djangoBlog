a57f129e4f26d7df576ecb8dddd2d601fac8e8f1
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2025-02-09 18:31:08 -0800

    added additional page for viewing a specific post.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -15,3 +15,7 @@ def post_list(request):
     
     # render(request, HTML_template, data for template to use)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk);
+    return render(request, 'blog/post_detail.html', {'post': post});
